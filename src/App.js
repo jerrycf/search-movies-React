569c6eef688d822df6eed949df7ab87c725b6655
jerrycf <superjerry95@gmail.com>
2021-02-23 18:48:51 -0600

    Checar linkeo a paginas/componentes en Detail.js y Movie.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React, {Component} from 'react'
+import {Switch, Route} from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+
 import './App.css';
 import 'bulma/css/bulma.css'
-import {Title} from './components/Title'
-import {SearchForm} from './components/SearchForm'
-import {MoviesList} from './components/MoviesList'
-
 
 class App extends Component {
-  state = {
-    results: [],
-    usedSearch: false
-  }
-  
-  _handleResults = (results) => {
-    this.setState({results: [], usedSearch: true})
-  }
-
-  _renderResults() {
-    return this.state.results.length === 0 
-      ? <p>No results found!</p>
-      : <MoviesList movies={this.state.results}/>
-  }
-  
   render() {
-    return (
+      return (
       <div className="App">
-        <Title>Search Movies</Title>
-        <div className='SearchForm-Wrapper'>
-          <SearchForm onResults={this._handleResults}/>
-        </div>
-        {this.state.usedSearch 
-          ? this._renderResults()
-          : <small>Use the form to seach for a Movie!</small>}
+        <Switch>
+          <Route exact path='/' Component={Home} />
+          <Route path='/detail/:id' Component={Detail} />
+        </Switch>
       </div>
     )
   }
---
+++ src/pages/Detail.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+const API_KEY = 'df3682fb'
+
+export class Detail extends Component {
+
+    static propTypes = {
+        match: PropTypes.shape({
+            params: PropTypes.object,
+            isExact: PropTypes.bool,
+            path: PropTypes.string,
+            url: PropTypes.string
+        })
+    }
+
+    state = {
+        movie: { }
+    }
+    _fetchMovie({id}) {
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
+            .then(res => res.json())
+            .then(movie => {
+                console.log({movie})
+                this.setState({movie})
+            })
+    }
+
+    componentDidMount() {
+        console.log(this.props)
+        const {id} = this.props.match.params
+        this._fetchMovie({id})
+    }
+
+    _goBack() {
+        window.history.back()
+    }
+
+    render(){ 
+        const { Title, Poster, Director, Actors, Metascore, Plot} = this.state.movie
+        return(
+            <div>
+                <button onClick={this._goBack}> HOME!</button>
+                <h1>{Title}</h1>
+                <img src={Poster} alt={Title} />
+                <strong>{Director}</strong>
+                <h3>{Actors}</h3>
+                <h4>{Metascore}</h4>
+                <p>{Plot}</p>
+            </div>
+        )
+    }
+}
